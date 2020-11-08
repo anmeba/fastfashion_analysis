54006172b1f822f81763d73e41a2f971fb3d949c
anmeba <54138803+anmeba@users.noreply.github.com>
2020-11-08 09:57:15 +0100

    Delete settings.py

--- venv/settings.py
+++
@@ -1,9 +0,0 @@
-# settings.py
-
-
-url = 'https://www.zara.com/sitemaps/sitemap-index.xml.gz'
-country = 'sitemap-es-es'
-gender = 'mujer'
-item_types = ['vestido', 'camiseta', 'blusa']
-keyword_script = 'detailedComposition'
-output = 50
